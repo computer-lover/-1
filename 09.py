0aeea94aabcfc1f670212f3bc450440e0fbe2b4d
computer-lover <2531244969@qq.com>
2019-09-19 11:17:14 +0800

    second commit

--- 09.py
+++ 09.py
@@ -1,4 +1,6 @@
 from datetime import datetime
-a=datetime.now()
-a.strftime('%Y-%m-%d %H:%M:%S')
-print(a.strftime)
+now1=datetime.now()
+now2=now1.strftime('%Y-%m-%d %H:%M:%S')
+print(now1)
+print(now2)
+
